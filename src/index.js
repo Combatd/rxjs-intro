cb41879e0ba19c88d27ecf52786c393ef7f17d39
Combatd <combatdstar@gmail.com>
2024-05-16 19:15:18 -0700

    Define optional events on Observable subscribe

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import { Observable } from "rxjs";
 
 const observable = new Observable((subscriber) => {
   subscriber.next('Hello world');
+  subscriber.error('Error!'); // if error event happens, observable terminated
+  subscriber.next('test');
+
+  subscriber.complete(); // terminates the observable preventing it from sending data
 });
 
 observable.subscribe({
   next: (value) => {
     console.log(value);
+  },
+
+  complete: () => {
+    console.log('complete called!');
+  },
+
+  error: (err) => {
+    console.log(err);
   }
 });
